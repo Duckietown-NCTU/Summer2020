864949c1a80ef9dd7a1b71873047c5ca54679f88
richtong <richtong888@gmail.com>
2020-08-17 15:22:02 +0800

    reuplaod

--- week6/Gazebo/gazebo_ws/catkin_ws/build/gazebo_ros_demos/gazebo_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/richtong/2020summerintern_week6/gazebo_ws/catkin_ws/src/gazebo_ros_demos/gazebo_tutorials
-# Build directory: /home/richtong/2020summerintern_week6/gazebo_ws/catkin_ws/build/gazebo_ros_demos/gazebo_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- week6/Gazebo/gazebo_ws/catkin_ws/build/gazebo_ros_demos/rrbot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/richtong/2020summerintern_week6/gazebo_ws/catkin_ws/src/gazebo_ros_demos/rrbot_description
-# Build directory: /home/richtong/2020summerintern_week6/gazebo_ws/catkin_ws/build/gazebo_ros_demos/rrbot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
